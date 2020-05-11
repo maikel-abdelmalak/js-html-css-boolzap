ed8fcb39c23aa752441c2ba6cecd370317ef0edb
maikel abdelmalak <maikel22@hotmail.it>
2020-05-11 18:13:34 +0200

    nuovo messaggio

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
-$(document).ready(){
+$(document).ready(function(){
+    $('.bottom input').focus(function(){
+        $('.bottom .fa-microphone').addClass('invisible');
+        $('.bottom .fa-google-play').removeClass('invisible');
+    });
+
+
+    $(document).click(function(event){
+        var target =  $(event.target);
+        if(!target.hasClass('i')){
+            $('.bottom .fa-microphone').removeClass('invisible');
+            $('.bottom .fa-google-play').addClass('invisible');
+            $('.bottom input').val('');
+        }
+
+    });
+
+    $('.fa-google-play').click(crea_messaggio);
+
+    function crea_messaggio(){
+        var testo = $('.bottom input').val();
+
+        if(testo != ''){
+            var nuovo_messaggio = $('.template .messaggio').clone();
+
+            $('.text-container').append(nuovo_messaggio);
+
+            $('.text-container .messaggio.inviati:last-child p').text(testo);
+
+            $('.bottom input').val('')
+
+        }
+
+
+    }
 
 
 
-
-
-
-}
+})
